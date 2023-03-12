3f275c61bd4f63e9aaa5c7c5c388143570b63cbd
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-03-12 14:05:22 +0200

    merge create api view with list api view

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from .views import (
-    api_home,
+    # api_home,
     # post_create,
-    PostDetailView, PostCreateAPIView)
+    PostDetailView,
+    PostListCreateAPIView
+)
+
+
 
 
 # app name
 app_name = 'blog'
 
 urlpatterns = [
-    path('', api_home, name='blog-home'),
+    # path('', PostListAPIView.as_view(), name='blog-home'),
     # path('post-create/', post_create, name='post-create'),
-    path('post-create/', PostCreateAPIView.as_view(), name='post-create'),
+    path('', PostListCreateAPIView.as_view(), name='post'),
     path('post-detail/<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,17 +1,15 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializers
 from rest_framework import generics
 
 
-@api_view(['GET'])
-def api_home(request, *args, **kwargs):
-    instance = Post.objects.all()
-    data = {}
-    if instance:
-        data = PostSerializers(instance, many=True).data
-    return Response(data)
+# @api_view(['GET'])
+# def api_home(request, *args, **kwargs):
+#     instance = Post.objects.all()
+#     data = {}
+#     if instance:
+#         data = PostSerializers(instance, many=True).data
+#     return Response(data)
 
 
 # @api_view(['POST'])
@@ -36,10 +34,27 @@ class PostDetailView(generics.RetrieveAPIView):
     # Post.objects.get(slug = slug)
 
 
-class PostCreateAPIView(generics.CreateAPIView):
+# class PostCreateAPIView(generics.CreateAPIView):
+#     '''
+#         Post Create API View
+#         - to create new instance from post
+#     '''
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializers
+
+#     def perform_create(self, serializer):
+
+#         # or get token send from serializer
+#         # token = serializer.validated_data['token']
+#         # user = Token.objects.get(token =token)
+#         # serializer.save(user = user)
+#         return super().perform_create(serializer)
+
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
     '''
-        Post Create API View
-        - to create new instance from post
+        Post List & Create API View
+        - to create new instance from post and get all item in post model
     '''
     queryset = Post.objects.all()
     serializer_class = PostSerializers
